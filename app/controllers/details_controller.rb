562281a819266724253be0bd4bd0fbaf82f2baba
svgiridharreddy <giridhar.reddy1992@gmail.com>
2019-07-04 15:10:43 +0530

    render individual category details

--- app/controllers/details_controller.rb
+++ app/controllers/details_controller.rb
@@ -6,8 +6,86 @@ class DetailsController < ApplicationController
 		@categories = {"AE Q1 Hotels Keywords":{},"Emirates - UAE Campaign":{},"India Flights":{},"India Hotels":{},"KSA Q1 Arabic Keywords":{},"KSA Q1 Keywords":{},"UAE Q1 Activities":{},"UAE Q1 Keywords":{},"Visa":{}}
 		categories_list = []
 		cat_obj ={}
-		categories_keys.each_with_index do |model,index|
-			case model
+		categories_keys.each_with_index do |key,index|
+			table_name = get_table_name(key)
+			obj = {}
+			@rank_one_keywords = table_name.where(google_rank: 1) rescue []
+			@rank_in_between_two_and_three = table_name.where(google_rank: 2..3) rescue []
+			@rank_in_between_four_and_ten = table_name.where(google_rank: 4..10) rescue []
+			@rank_in_between_ten_and_twenty = table_name.where(google_rank: 10..20) rescue []
+			@rank_above_twenty = table_name.where("google_rank > ?",20 ) rescue []
+			@categories["#{key}"] = {"rank_one_keywords" => @rank_one_keywords.count,
+				"rank_in_between_two_and_three" => @rank_in_between_two_and_three.count,
+				"rank_in_between_four_and_ten"=>@rank_in_between_four_and_ten.count,
+				"rank_in_between_ten_and_twenty"=>@rank_in_between_ten_and_twenty.count,
+				"rank_above_twenty"=>@rank_above_twenty.count}
+				categories_list << key
+			end
+			list = {}
+			list["categories_keys"] = categories_list
+			list["categories_data"] = @categories
+			render json: list
+		end
+
+		def category_details
+			@category_name = params[:category_name].split("_").join(" ")
+			categories_list  = HTTParty.get("https://serpbook.com/serp/api/?action=getcategories&auth=d3f28ee6533cfffa743ce5630ca35600")
+			categories_keys = categories_list.keys
+			category_exist = categories_keys.include?(@category_name)
+			column_headings = []
+			table_headings = {"category_name" => "Category Name","tags" => "Tag","keyword" => "Keyword","start_date" => "Start Date","current_date" => "Current Date"}
+			if category_exist
+				table_name = get_table_name(@category_name)
+				keywords = table_name.all.pluck(:keyword).uniq
+				main_obj = []
+				start_date_ranks = {"mobile_rank"=> "","desktop_rank" => ""}
+				current_date_ranks = {"mobile_rank"=> "","desktop_rank" => ""}
+
+				keywords.each do |kw|
+				start_date_records = table_name.where("keyword=? and Date(created_at) = ?",kw, "2019-07-02")
+				first_record  = start_date_records.first
+				category_name = first_record.category_name
+				tag = first_record.tags
+				keyword = first_record.keyword
+
+				start_date_records.each do |sr|
+					if sr.search_type == "sem"
+						start_date_ranks["mobile_rank"] = sr.google_rank
+					else
+						start_date_ranks["desktop_rank"] = sr.google_rank
+					end
+
+				end
+					current_date_records = table_name.where("keyword=? and Date(created_at) = ? ",kw, Date.today.to_s(:db))
+					current_date_records.each do |cr|
+						if cr.search_type == "sem"
+							current_date_ranks["mobile_rank"] = cr.google_rank
+						else
+							current_date_ranks["desktop_rank"] = cr.google_rank
+						end
+					end
+					main_obj << {"category_name" => category_name,"tags" => tag,"keyword" => keyword,"start_date_ranks" =>start_date_ranks,"current_date_ranks" => current_date_ranks }
+
+					# mobile_start_date_records = table_name.where("keyword=? and Date(created_at) = ? and search_type='sem'",kw, "2019-07-02")
+					# mobile_current_date_records = table_name.where("keyword=? and Date(created_at) = ?  and search_type='sem'", Date.today.to_s(:db))
+				end
+			
+				start_date = eval(table_name.group(:keyword).first.last_update)["date"]
+				current_date = Date.today.to_formatted_s(:long_ordinal)
+				table_columns = table_name.column_names
+				table_columns.push("start_date","current_date")
+				heading_obj = {}
+				table_columns.each do |tc|
+					if table_headings["#{tc}"].present?  
+					heading_obj = {"accessor" => "#{tc}","heading" => table_headings["#{tc}"]}
+					column_headings << heading_obj
+					end
+				end
+			end
+			render json: {main_obj:  main_obj,headings: column_headings}
+		end
+		def get_table_name(key)
+			case key
 			when "AE Q1 Hotels Keywords"
 				table_name = Aeq1HotelsKeyword
 			when "Emirates - UAE Campaign"
@@ -29,58 +107,5 @@ class DetailsController < ApplicationController
 			else
 				table_name = ""			
 			end
-			obj = {}
-			@rank_one_keywords = table_name.where(google_rank: 1)
-			@rank_in_between_two_and_three = table_name.where(google_rank: 2..3)
-			@rank_in_between_four_and_ten = table_name.where(google_rank: 4..10)
-			@rank_in_between_ten_and_twenty = table_name.where(google_rank: 4..10)
-			@rank_above_twenty = table_name.where("google_rank > ?",20 )
-			@categories["#{model}"] = {"rank_one_keywords" => @rank_one_keywords.count,
-				"rank_in_between_two_and_three" => @rank_in_between_two_and_three.count,
-				"rank_in_between_four_and_ten"=>@rank_in_between_four_and_ten.count,
-				"rank_in_between_ten_and_twenty"=>@rank_in_between_ten_and_twenty.count,
-				"rank_above_twenty"=>@rank_above_twenty.count}
-				categories_list << model
-			end
-			list = {}
-			list["categories_keys"] = categories_list
-			list["categories_data"] = @categories
-			render json: list
-		end
-		def fetch_details
-			@category_name = params[:category_name]
-			categories_list  = HTTParty.get("https://serpbook.com/serp/api/?action=getcategories&auth=d3f28ee6533cfffa743ce5630ca35600")
-			categories_keys = categories_list.keys
-			category_exist = categories_keys.include?(@category_name)
-			if category_exist
-				case @category_name
-				when "AE Q1 Hotels Keywords"
-					table_name = Aeq1HotelsKeyword
-				when "Emirates - UAE Campaign"
-					table_name = EmiratesUaeCampaign
-				when "India Flights"
-					table_name = IndiaFlight
-				when "India Hotels"
-					table_name = IndiaHotel
-				when "KSA Q1 Arabic Keywords"
-					table_name = Ksaq1ArabicKeyword
-				when "KSA Q1 Keywords"
-					table_name = Ksaq1Keyword
-				when "UAE Q1 Activities"
-					table_name = Uaeq1Activity
-				when "UAE Q1 Keywords"
-					table_name=Uaeq1Keyword
-				when "Visa"
-					table_name = Visa
-				else
-					table_name = ""			
-				end
-				@rank_one_keywords = table_name.where(google_rank: 1)
-				@rank_in_between_two_and_three = table_name.where(google_rank: 2..3)
-				@rank_in_between_four_and_ten = table_name.where(google_rank: 4..10)
-				@rank_in_between_ten_and_twenty = table_name.where(google_rank: 4..10)
-				@rank_above_twenty = table_name.where("google_rank > ?",20 )
-			end
-
 		end
 	end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 	resources :details
 	get 'dashboard' => "details#dashboard"
-	get 'category/:category_name' => "details#fetch_details"
+	get 'category/:category_name' => "details#category_details"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
