7689c68b7c2cb8641de6213e23205d5e6364555c
svgiridharreddy <giridhar.reddy1992@gmail.com>
2019-07-18 16:35:44 +0530

    changes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 	get 'dashboard' => "details#dashboard"
 	get 'category/:category_name' => "details#category_details"
 	get 'overall_categories' => "details#overall_categories_view"
-	get '*path' => redirect('/dashboard')
+	get '*path' => redirect('/overall_categories_view')
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
  
