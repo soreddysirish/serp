86b8203ee92b6155aee52ea441023737cea6912f
svgiridharreddy <giridhar.reddy1992@gmail.com>
2019-07-29 18:56:02 +0530

    individual category bottom pagination

--- app/controllers/details_controller.rb
+++ app/controllers/details_controller.rb
@@ -287,7 +287,8 @@ class DetailsController < ApplicationController
 					data={ name:params["username"] }
 					pay_load={data:data,exp: (Time.now+10.minutes).to_i}
 					token = JWT.encode(pay_load,secret,'HS256')
-					render json: {token:token},status:200
+					binding.pry
+					render json: {token:token},status: 200
 				end
 			else
 				render json:{error: 'unauthorised user'},status: 500
